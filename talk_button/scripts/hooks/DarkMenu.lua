ad9409df62850b4cc61d61327c68a9272174c73e
Nell <alex.cimer@mail.ru>
2026-09-11 01:39:04 +0300

    v 1.3.0

--- talk_button/scripts/hooks/DarkMenu.lua
+++ talk_button/scripts/hooks/DarkMenu.lua
@@ -1,5 +1,5 @@
 ---@class DarkMenu
-local DarkMenu, super = Class("DarkMenu", true)
+local DarkMenu, super = HookSystem.hookScript(DarkMenu)
 
 function DarkMenu:getButtonSpacing()
     if #self.buttons <= 4 then
@@ -10,55 +10,10 @@ function DarkMenu:getButtonSpacing()
 end
 
 function DarkMenu:addButtons()
-    -- ITEM
-    self:addButton({
-        ["state"]          = "ITEMMENU",
-        ["sprite"]         = Assets.getTexture("ui/menu/btn/item"),
-        ["hovered_sprite"] = Assets.getTexture("ui/menu/btn/item_h"),
-        ["desc_sprite"]    = Assets.getTexture("ui/menu/desc/item"),
-        ["callback"]       = function()
-            self.box = DarkItemMenu()
-            self.box.layer = 1
-            self:addChild(self.box)
+    super.addButtons(self)
 
-            self.ui_select:stop()
-            self.ui_select:play()
-        end
-    })
-
-    -- EQUIP
-    self:addButton({
-        ["state"]          = "EQUIPMENU",
-        ["sprite"]         = Assets.getTexture("ui/menu/btn/equip"),
-        ["hovered_sprite"] = Assets.getTexture("ui/menu/btn/equip_h"),
-        ["desc_sprite"]    = Assets.getTexture("ui/menu/desc/equip"),
-        ["callback"]       = function()
-            self.box = DarkEquipMenu()
-            self.box.layer = 1
-            self:addChild(self.box)
-
-            self.ui_select:stop()
-            self.ui_select:play()
-        end
-    })
-
-    -- POWER
-    self:addButton({
-        ["state"]          = "POWERMENU",
-        ["sprite"]         = Assets.getTexture("ui/menu/btn/power"),
-        ["hovered_sprite"] = Assets.getTexture("ui/menu/btn/power_h"),
-        ["desc_sprite"]    = Assets.getTexture("ui/menu/desc/power"),
-        ["callback"]       = function()
-            self.box = DarkPowerMenu()
-            self.box.layer = 1
-            self:addChild(self.box)
-
-            self.ui_select:stop()
-            self.ui_select:play()
-        end
-    })
-    --Only add if not alone(If the config is false)
-    if (Kristal.getLibConfig("talk_button", "have_talk_when_alone") or #Game.world.followers > 0) then
+    -- only add if not alone (if the config is false)
+    if Kristal.getLibConfig("talk_button", "have_talk_when_alone") or #Game.world.followers > 0 then
         -- TALK
         self:addButton({
             ["state"]          = "TALK",
@@ -74,24 +29,8 @@ function DarkMenu:addButtons()
 
                 Game.world:startCutscene("_talk")
             end
-        })
+        }, Kristal.getLibConfig("talk_button", "talk_button_position"))
     end
-
-    -- CONFIG
-    self:addButton({
-        ["state"]          = "CONFIGMENU",
-        ["sprite"]         = Assets.getTexture("ui/menu/btn/config"),
-        ["hovered_sprite"] = Assets.getTexture("ui/menu/btn/config_h"),
-        ["desc_sprite"]    = Assets.getTexture("ui/menu/desc/config"),
-        ["callback"]       = function()
-            self.box = DarkConfigMenu()
-            self.box.layer = -1
-            self:addChild(self.box)
-
-            self.ui_select:stop()
-            self.ui_select:play()
-        end
-    })
 end
 
 return DarkMenu
